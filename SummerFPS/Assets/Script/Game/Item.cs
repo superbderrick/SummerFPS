f5dafa4a05262c1e62df729aa68aec358c83ea4a
Derrick <kang.derrick@gmail.com>
2022-07-14 15:26:14 +0900

    Added game

---
+++ SummerFPS/Assets/Script/Game/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+	public ItemInfo itemInfo;
+	public GameObject itemGameObject;
+
+	public abstract void Use();
+}
