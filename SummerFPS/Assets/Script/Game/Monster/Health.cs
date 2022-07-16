14ffbd94a34ade68d2af0be8164105539cc774d4
Derrick <kang.derrick@gmail.com>
2022-07-16 21:30:58 +0900

    basic implemntaion

---
+++ SummerFPS/Assets/Script/Game/Monster/Health.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health = 1000f;
+    
+    public void TakeDamage(float amount)
+    {
+        
+        Debug.Log("amount " + amount);
+        health -= amount;
+        if(health <= 0f)
+        {
+            
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Die ");
+        Destroy(gameObject);
+    }
+
+}
