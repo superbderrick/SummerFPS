535fc47e45c546b217bb75009f8d42ec9a1048d8
Derrick <kang.derrick@gmail.com>
2022-07-17 19:55:38 +0900

    improved

--- SummerFPS/Assets/Script/InGame/Player/Gun/SingleShotGun.cs
+++ SummerFPS/Assets/Script/InGame/Player/Gun/SingleShotGun.cs
@@ -8,7 +8,7 @@ public class SingleShotGun : Gun
     [SerializeField] Camera cam;
 
     PhotonView PV;
-    private bool canShoot = true;
+    
     [SerializeField, Range(0, 1f)] private float fireRate;
     
     void Awake()
@@ -18,73 +18,45 @@ public class SingleShotGun : Gun
 
     public override void Use()
     {
-        //Shoot();
         StartCoroutine(Fire());
     }
     
     private IEnumerator Fire()
     {
-        canShoot = false;
-        yield return new WaitForSeconds(fireRate);
-        var shot = ShotPool.Instance.Get();
-        shot.transform.position = new Vector3(transform.position.x - 0.1f,transform.position.y,transform.position.z);
-        shot.transform.rotation = transform.rotation;
-        
-        shot.gameObject.SetActive(true);
-        canShoot = true;
-        PV.RPC("TakeHitRPC", RpcTarget.All);
-        
-        
+        // canShoot = false;
+         yield return new WaitForSeconds(0.125f);
+         PV.RPC("TakeHitRPC", RpcTarget.All);
     }
     
-
-    void Shoot()
-    {
-        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        ray.origin = cam.transform.position;
-        if(Physics.Raycast(ray, out RaycastHit hit))
-        {
-            Debug.Log("raytranfrom name " + hit.transform);
-            hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
-            Health enemyHealth = hit.transform.GetComponent<Health>();
-            if(enemyHealth != null)
-            {
-                Debug.Log("Shoot ");
-                enemyHealth.TakeDamage(500);
-            }
-            
-        }
-    }
-
-  
     [PunRPC]
     public IEnumerator TakeHitRPC()
     {
-        canShoot = false;
-        yield return new WaitForSeconds(fireRate);
         var shot = ShotPool.Instance.Get();
-        shot.transform.position = new Vector3(transform.position.x - 0.1f,transform.position.y,transform.position.z);
+        
+        Debug.Log("transform.position derrick x " + transform.position.x);
+        Debug.Log("transform.position derrick y " + transform.position.y);
+        Debug.Log("transform.position derrick z " + transform.position.z);
+        
+        shot.transform.position = transform.position;
         shot.transform.rotation = transform.rotation;
-        
         shot.gameObject.SetActive(true);
-        canShoot = true;
         
-          
+        
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
         ray.origin = cam.transform.position;
         if(Physics.Raycast(ray, out RaycastHit hit))
         {
-            Debug.Log("raytranfrom name " + hit.transform);
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
             Health enemyHealth = hit.transform.GetComponent<Health>();
             if(enemyHealth != null)
             {
-                Debug.Log("Shoot ");
                 enemyHealth.TakeDamage(500);
             }
             
         }
-       
+
+        yield return null;
+
     }
     
 
