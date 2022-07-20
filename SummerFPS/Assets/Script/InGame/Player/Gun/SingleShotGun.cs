3d5ea43908d3512b60f5a3aaf6b4b936ba268c63
Derrick <kang.derrick@gmail.com>
2022-07-20 12:37:45 +0900

    Fixed bullet feature

--- SummerFPS/Assets/Script/InGame/Player/Gun/SingleShotGun.cs
+++ SummerFPS/Assets/Script/InGame/Player/Gun/SingleShotGun.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class SingleShotGun : Gun
 {
-    [SerializeField] Camera cam;
+    [SerializeField]
+    Camera cam;
 
     PhotonView PV;
-    [SerializeField, Range(0, 1f)] private float fireRate;
-    [SerializeField ] private int power;
-    
+    [SerializeField, Range(0, 1f)]
+    private float fireRate;
+    [SerializeField]
+    private int power;
+    public Transform spawnPoint;
+
     void Awake()
     {
         PV = GetComponent<PhotonView>();
@@ -20,36 +24,34 @@ public class SingleShotGun : Gun
     {
         StartCoroutine(Fire());
     }
-    
+
     private IEnumerator Fire()
     {
         yield return new WaitForSeconds(fireRate);
-         PV.RPC("TakeHitRPC", RpcTarget.All);
+        PV.RPC("TakeHitRPC", RpcTarget.All);
     }
-    
+
     [PunRPC]
     public IEnumerator TakeHitRPC()
     {
         var shot = ShotPool.Instance.Get();
-        
-        shot.transform.position = transform.position;
+
+        shot.transform.position = spawnPoint.position;
         shot.transform.rotation = transform.rotation;
         shot.gameObject.SetActive(true);
-        
+
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
         ray.origin = cam.transform.position;
-        if(Physics.Raycast(ray, out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
             MonsterManager enemyMonsterManager = hit.transform.GetComponent<MonsterManager>();
-            if(enemyMonsterManager != null)
+            if (enemyMonsterManager != null)
             {
                 enemyMonsterManager.TakeDamage(power);
             }
-            
         }
+
         yield return null;
     }
-    
-
 }
